eb2835f68fb32825255917358b9bea4e7148d7d0
RobSorce <srcrbt@gmail.com>
2019-03-20 19:56:36 +0100

    updated apf node, included attractive force field, created node with main functions

---
+++ src/apf_planner_node.cpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <../include/apf_planner.h>
+
+int main(int argc, char** argv)
+{
+
+    ros::init(argc, argv, "apf_planner");
+
+    ros::NodeHandle nh;
+
+    apf_motion_planner apf_planner(nh);
+
+    apf_planner.init();
+
+    ros::spin();
+
+    return 0;
+
+}
